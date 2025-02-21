cc37f0a9ab17984dcc8a1ece3c30ad7b1a793275
Muse <musemy8484@gmail.com>
2025-02-21 11:03:36 +0300

    removed unwanted lines

--- src/app/(auth-routes)/signin/page.tsx
+++ src/app/(auth-routes)/signin/page.tsx
@@ -1,9 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import SignIn from "./_components/signin-button";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Mail } from "lucide-react";
-import { SelectSeparator } from "@/components/ui/select";
 
 const SignInPage = () => {
   return (
@@ -14,18 +10,6 @@ const SignInPage = () => {
         </CardTitle>
       </CardHeader>
       <CardContent className="w-96 flex flex-col gap-4">
-        {/* <div className="mt-10 flex flex-col gap-4">
-          <Input placeholder="your@email.com" />
-          <Button variant="outline">
-            <Mail className="mr-2 w-5 h-5" />
-            Continue with Email
-          </Button>
-        </div>
-        <div className="flex items-center gap-2">
-          <div className="grow basis-0 border-b border-input" />
-          <p className="text-sm">OR</p>
-          <div className="grow basis-0 border-b border-input" />
-        </div> */}
         <div className="mt-10">
           <SignIn />
         </div>
