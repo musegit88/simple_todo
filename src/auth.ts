d839cadba1183cdc679c1bddfaee5efb841ce1f4
muse <103637246+musegit88@users.noreply.github.com>
2025-06-03 13:16:25 +0300

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -90,6 +90,6 @@ if (!googleAccount.expires_at || googleAccount.expires_at * 1000 < Date.now()) {
 
 declare module "next-auth" {
   interface Session {
-    error?: "RefreshError";
+    error?: "RefreshError" || "NoGoogleAccount" || "Missing google tokens";
   }
 }
