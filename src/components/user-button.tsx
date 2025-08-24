584287f5f80f3e17d6dc9c217647fcd9dde7133e
Muse <musemy8484@gmail.com>
2025-08-24 17:34:13 +0300

    fix: using global state for sidebar opening & closing state

--- src/components/user-button.tsx
+++ src/components/user-button.tsx
@@ -14,8 +14,10 @@ import {
 import { Button } from "@/components/ui/button";
 import { UserComponentProps } from "@/types";
 import Link from "next/link";
+import { useSidebar } from "@/hooks/useSidebar";
 
 const UserButton = ({ user }: UserComponentProps) => {
+  const sidebar = useSidebar();
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
@@ -43,7 +45,7 @@ const UserButton = ({ user }: UserComponentProps) => {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-64">
-        <Link href={"/settings"}>
+        <Link href={"/settings"} onClick={sidebar.onClose}>
           <DropdownMenuItem>
             <div className="flex items-center gap-2">
               <Settings size={18} />
