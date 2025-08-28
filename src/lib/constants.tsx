ad05d321322c3903b807bede0c2dd6f8e599aaaa
Muse <musemy8484@gmail.com>
2025-08-28 04:39:55 +0300

    update navigation link

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -1,7 +1,7 @@
 import { CalendarPlus2, CircleCheck, FileText, Home, Star } from "lucide-react";
 
 export const links = [
-  { title: "Tasks", href: "/", icon: Home, color: "#14b8a6" },
+  { title: "Tasks", href: "/home", icon: Home, color: "#14b8a6" },
   { title: "My Day", href: "/my-day", icon: FileText, color: "#6366f1" },
   {
     title: "Important",
