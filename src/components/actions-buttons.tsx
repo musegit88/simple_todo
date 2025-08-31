8c29181acb60c55fec4d4020db93b19a0db28cc9
Muse <musemy8484@gmail.com>
2025-08-31 16:56:05 +0300

    style: cancle action button

--- src/components/actions-buttons.tsx
+++ src/components/actions-buttons.tsx
@@ -3,6 +3,7 @@
 import { useTaskIds } from "../hooks/useTaskIds";
 import DeleteTasks from "./delete-tasks";
 import ImportantTasks from "./important-tasks";
+import { Button } from "./ui/button";
 
 const ActionButtons = () => {
   const { taskIds, setTaskIds } = useTaskIds();
@@ -10,15 +11,16 @@ const ActionButtons = () => {
   return (
     <>
       {taskIds.length > 1 && (
-        <div className="flex gap-2">
+        <div className="flex items-center gap-2">
           <DeleteTasks />
           <ImportantTasks />
-          <div
+          <Button
             onClick={() => setTaskIds([])}
-            className="text-xs sm:text-base border border-input bg-background hover:bg-accent hover:text-accent-foreground w-fit px-2 py-1 rounded-sm flex items-center gap-1 cursor-pointer"
+            className="text-xs sm:text-base text-black dark:text-white border border-input bg-background hover:bg-accent hover:text-accent-foreground w-fit px-2 py-1 rounded-sm flex items-center gap-1 cursor-pointer"
+            size="sm"
           >
             Cancle
-          </div>
+          </Button>
         </div>
       )}
     </>
