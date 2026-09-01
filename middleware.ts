dd7e5b35bc9e3b3d11b08fa3bab606aea829fe79
Muse <musemy8484@gmail.com>
2026-09-01 10:59:49 +0300

    feat: implement middleware to redirect from /home to /tasks

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,11 @@
-// export { auth as middleware } from "./auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/home") {
+    return NextResponse.redirect(new URL("/tasks", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/home"],
+};
